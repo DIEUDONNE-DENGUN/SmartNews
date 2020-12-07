8b2a7dfba08ce094609eab374c5ee8049e11d98e
Dieudonne Dengun <dieudonnenjifor@gmail.com>
2020-12-07 11:24:02 +0100

    Update const.py

--- smartnews/const.py
+++ smartnews/const.py
@@ -1,10 +1,10 @@
 # Hold entire application constants
 
-APP_SECRET_KEY ="33191185783744c6a127b20c396a67fb"
+APP_SECRET_KEY =""
 DAILY_NEWS_BASE_URL = "http://237dailynews.com/"
 NEWS_ORG_API_BASE_URL = "https://newsapi.org/v2/"
 NEWS_AVATAR_BASE_URL = "https://avatars.io/facebook/"
-NEWS_ORG_API_KEY = "902bf27e4c9540f6b851ad229e1457f0"
+NEWS_ORG_API_KEY = ""
 NEWS_ORG_API_DEFAULT_COUNTRY = "us"
 NEWS_ORG_API_CATEGORIES = ["general", "business",
                            "entertainment", "health", "sports", "science", "technology"]
